78698c9e019d325ae2480b91850df0890fcc74b5
Brittney Haselmayer <brittneyhaselmayer@yahoo.com>
2019-10-23 21:16:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,12 +5,11 @@
 def load_library (file)
   file = YAML.load_file("./lib/emoticons.yml")
    
- 
   new_hash = {
    "get_meaning" => {},
    "get_emoticon" => {}
  }
- library.each do |meaning, translation|
+ file.each do |meaning, translation|
    english = translation[0]
    japanese = translation[1]
    new_hash["get_meaning"][japanese] = meaning
